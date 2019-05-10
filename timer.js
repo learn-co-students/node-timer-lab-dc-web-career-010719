7e6c92e2b5a042be6384aa334fbd8de2178a18e6
James Clement <jameseclement@gmail.com>
2019-05-10 15:26:39 -0400

    input can be either minutes or seconds, done

--- timer.js
+++ timer.js
@@ -3,10 +3,14 @@ let input = process.argv[2];
 let unit = input[input.length - 1];
 let time = parseInt(input);
 
-let int = setInterval(() => {
-    time -= 1;
-    console.log(`Left: ${time}s`);
-  }, 1000);
+if (unit === "n") {
+  time *= 60;
 }
-global.clearInterval();
-process.exit();
+let int = setInterval(() => {
+  time -= 1;
+  console.log(`Left: ${time}s`);
+  if (time === 0) {
+    clearInterval(int);
+    process.exit();
+  }
+}, 1000);
